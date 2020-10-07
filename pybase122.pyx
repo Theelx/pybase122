3b3daf86d1bc8e875458dc1433188474e280cd1b
Theelgirl <43764914+Theelgirl@users.noreply.github.com>
2020-10-07 18:29:40 +0000

    2x faster
    Thanks so much godlygeek#8814 from Discord!

--- pybase122.pyx
+++ pybase122.pyx
@@ -1,16 +1,30 @@
 # distutils: language=c++
 cimport cython
-from libc.stdlib cimport abort, malloc, free
 import sys
 import time  # for performance timing
-from libcpp.string cimport string
-from libcpp.list cimport list as cpplist
 from libcpp.vector cimport vector
 
 PY2 = sys.version_info[0] == 2
 
 class SpeedException(Exception):
     pass
+
+# null, newline, carriage return, double quote, ampersand, backslash
+cdef int[6] illegalByteByIndex
+illegalByteByIndex[0] = ord('\0')
+illegalByteByIndex[1] = ord('\n')
+illegalByteByIndex[2] = ord('\r')
+illegalByteByIndex[3] = ord('"')
+illegalByteByIndex[4] = ord('&')
+illegalByteByIndex[5] = ord('\\')
+
+cdef int[256] illegalIndexByByte
+cdef int i
+
+for i in range(256):
+    illegalIndexByByte[i] = -1
+for i in range(len(illegalByteByIndex)):
+    illegalIndexByByte[illegalByteByIndex[i]] = i
 
 cdef int rshift(long int val, long int n) nogil:
     if val >= 0:
@@ -21,7 +35,7 @@ cdef int rshift(long int val, long int n) nogil:
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cdef (int, int, int) get7(int rawDataLen, int curIndex, int curBit, bytearray rawDataBytes):
+cdef (int, int, int) get7(int rawDataLen, int curIndex, int curBit, bytearray rawDataBytes) nogil:
     cdef int firstPart, secondPart
 
     if curIndex >= rawDataLen:
@@ -42,25 +56,26 @@ cdef (int, int, int) get7(int rawDataLen, int curIndex, int curBit, bytearray ra
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cdef tuple push7(int byte, int curByte, int bitOfByte, list decoded):
+cdef (int, int) push7(int byte, int curByte, int bitOfByte, vector[int]& decoded):
     byte <<= 1
     curByte |= rshift(byte, 0x100000000) >> bitOfByte
     bitOfByte += 7
     if bitOfByte >= 8:
-        decoded.append(curByte)
+        decoded.push_back(curByte)
         bitOfByte -= 8
         curByte = (byte << (7 - bitOfByte)) & 255
-    return curByte, bitOfByte, decoded
+    return (curByte, bitOfByte)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cpdef bytearray encode(str rawData, bint warnings=True, bint speed=True):
+cpdef bytearray encode(str rawData, bint warnings=True):
     cdef int curIndex, curBit, rawDataLen, illegalIndex, kShortened, b1, b2, bits, nextBits
     cdef bytearray firstPart
     cdef int firstBit
-    cdef cpplist[int] outData
+    #cdef list kIllegals
     cdef bytearray rawDataBytes
+    outData = bytearray()
 
     if PY2 and warnings:
         raise NotImplementedError("This hasn't been tested on Python2 yet! Turn this warning off by passing warnings=False.")
@@ -68,30 +83,21 @@ cpdef bytearray encode(str rawData, bint warnings=True, bint speed=True):
         rawDataBytes = bytearray(rawData, "UTF-8")
     else:
         raise TypeError("rawData must be a string!")
-    # null, newline, carriage return, double quote, ampersand, backslash
-    if speed:
-        kIllegals = {chr(0), chr(10), chr(13), chr(34), chr(38), chr(92)}
-    else:
-        kIllegals = [chr(0), chr(10), chr(13), chr(34), chr(38), chr(92)]
     kShortened = 0b111  # last two-byte char encodes <= 7 bits
     curIndex = curBit = 0
 
     #    for loops don't work because they cut off a variable amount of end letters for some reason, but they'd speed it up immensely
+    #    for i in range(len(rawDataBytes)):
     while True:
         bits, curIndex, curBit = get7(len(rawDataBytes), curIndex, curBit, rawDataBytes)
-        #bits, curIndex, curBit = no[0], no[1], no[2]
         if not bits:
             break
-        if bits in kIllegals:
-            if speed:
-                raise SpeedException("Encode is much faster without allowing illegal characters! Pass speed=False to disable this.")
-            else:
-                illegalIndex = kIllegals.index(bits)
-        else:
-            outData.push_back(bits)
+        illegalIndex = illegalIndexByByte[bits]
+        if illegalIndex == -1:
+            outData.append(bits)
             continue
+
         nextBits, curIndex, curBit = get7(len(rawDataBytes), curIndex, curBit, rawDataBytes)
-        #nextBits, curIndex, curBit = no[0], no[1], no[2]
         b1 = 0b11000010
         b2 = 0b10000000
         if not nextBits:
@@ -102,24 +108,22 @@ cpdef bytearray encode(str rawData, bint warnings=True, bint speed=True):
         firstBit = 1 if (nextBits & 0b01000000) > 0 else 0
         b1 |= firstBit
         b2 |= nextBits & 0b00111111
-        outData.push_back(b1)
-        outData.push_back(b2)
-    return bytearray(outData)
+        outData.append(b1)
+        outData.append(b2)
+    return outData
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 cpdef str decode(bytearray strData, bint warnings=True):
-    cdef list kIllegals = [0, 10, 13, 34, 38, 92]
-    cdef list decoded = []
-    cdef bytes illegalIndex
+    cdef vector[int] decoded
+    cdef int illegalIndex
     cdef int decodedIndex, curByte, bitOfByte, kShortened, int_strData
 
     if PY2 and warnings:
         raise NotImplementedError(
             "This hasn't been tested on Python2 yet! Turn this warning off by passing warnings=False."
         )
-    # null, newline, carriage return, double quote, ampersand, backslash
     kShortened = 0b111  # last two-byte char encodes <= 7 bits
     decodedIndex = curByte = bitOfByte = 0
 
@@ -127,13 +131,17 @@ cpdef str decode(bytearray strData, bint warnings=True):
     if not isinstance(strData[0], int):
         raise TypeError("You can only decode an encoded string!")
 
-    for i in range(len(strData)):
-        int_strData = int(strData[i])
+    cdef int i = 0
+    while i < len(strData):
+        int_strData = <int>(strData[i])
+        i += 1
         if int_strData > 127:
-            illegalIndex = (rshift(int_strData, 0x100000000) >> 8) & 7
+            int_strData = (int_strData & 0b00011111) << 6 | (<int>(strData[i]) & 0b00111111)
+            i += 1
+            illegalIndex = (int_strData >> 8) & 7
             if illegalIndex != kShortened:
-                curByte, bitOfByte, decoded = push7(chr(kIllegals[illegalIndex]), curByte, bitOfByte, decoded)
-            curByte, bitOfByte, decoded = push7(int_strData & 127, curByte, bitOfByte, decoded)
+                curByte, bitOfByte = push7(illegalByteByIndex[illegalIndex], curByte, bitOfByte, decoded)
+            curByte, bitOfByte = push7(int_strData & 127, curByte, bitOfByte, decoded)
         else:
-            curByte, bitOfByte, decoded = push7(int_strData, curByte, bitOfByte, decoded)
-    return "".join([chr(letter) for letter in decoded])
+            curByte, bitOfByte = push7(int_strData, curByte, bitOfByte, decoded)
+    return "".join(map(chr, decoded))
