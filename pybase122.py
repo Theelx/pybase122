6526549caecf01c8ad84595446f33142bc7d0996
Theelx <43764914+Theelx@users.noreply.github.com>
2022-08-18 22:52:32 -0400

    Remove useless prints

--- pybase122.py
+++ pybase122.py
@@ -42,20 +42,14 @@ def encode(rawData, warnings=True):
     # for loops don't work because they cut off a variable amount of end letters for some reason, but they'd speed it up immensely
     while True:
         retBits, bits = get7(len(rawData))
-        print("bits:", bits)
-        print("retBits:", retBits)
         if not retBits:
             break
         if bits in kIllegalsSet:
             illegalIndex = kIllegals.index(bits)
-            #print(bits, kIllegalsSet, illegalIndex)
         else:
-            #print(bits, kIllegalsSet)
             outData.append(bits)
             continue
         retNext, nextBits = get7(len(rawData))
-        print("retNext:", retNext)
-        print("nextBits:", nextBits)
         b1 = 0b11000010
         b2 = 0b10000000
         if not retNext:
@@ -66,9 +60,7 @@ def encode(rawData, warnings=True):
         firstBit = 1 if (nextBits & 0b01000000) > 0 else 0
         b1 |= firstBit
         b2 |= nextBits & 0b00111111
-        print("b1b2", b1, b2)
         outData += [b1, b2]
-    print(outData)
     return outData
 
 
